0dcdabf211875d1e41aba61852d40018ba68b5d6
Michele Santana <78629902+micheleclarice@users.noreply.github.com>
2026-04-16 14:31:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
 import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    minify: 'esbuild'
+  }
+})
+  import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
